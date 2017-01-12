6eea63db97dca38b7c4a30c2dc1ccff2616bafc8
Andrew Van Beek <andrewvanbeek90gmail.com>
2017-01-12 11:31:15 -0600

    fixes photo troubleshooting problem

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,8 +3,9 @@ class GamesController < ApplicationController
     doc = Nokogiri::HTML(open("https://twitter.com/i/streams/category/686639666779394060"))
     doc = doc.css("#doc")
     @body = doc.to_s
-    tweet_results = $twitter.search("#nintendo -rt", filter: "images", result_type: "recent").take(36).to_a.shuffle
+    tweet_results = $twitter.search("#sony -rt", filter: "images", result_type: "recent").take(36).to_a.shuffle
     @tweets = tweet_results.take(9)
+    @backgrounds = ["streetbackground.png", "industrial-background.jpg", "forestpassage.png", "forestbg.png", "BatmanArkhamKnightGothamCity.jpg", "creek.png", "cove.png", "chicago.png", "bridge.jpg"]
   end
 
   def create
