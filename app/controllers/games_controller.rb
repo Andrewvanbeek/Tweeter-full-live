8e8ee2bbf8dba37ef90472ec47355e3e3f973da0
Andrew Van Beek <andrewvanbeek90gmail.com>
2017-01-12 15:11:06 -0600

    layout works like a charm now

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,8 @@
 class GamesController < ApplicationController
   def index
-    doc = Nokogiri::HTML(open("https://twitter.com/i/streams/category/686639666779394060"))
-    doc = doc.css("#doc")
-    @body = doc.to_s
-    tweet_results = $twitter.search("#sony -rt", filter: "images", result_type: "recent").take(36).to_a.shuffle
-    @tweets = tweet_results.take(9)
-    @backgrounds = ["streetbackground.png", "industrial-background.jpg", "forestpassage.png", "forestbg.png", "BatmanArkhamKnightGothamCity.jpg", "creek.png", "cove.png", "chicago.png", "bridge.jpg"]
+    tweet_results = $twitter.search("sony -rt", result_type: "recent", lang: "en").to_a.shuffle
+    @tweets = tweet_results.take(tweet_results.length / 2)
+    @backgrounds = ["streetbackground.png", "industrial-background.jpg", "forestpassage.png", "forestbg.png", "BatmanArkhamKnightGothamCity.jpg", "creek.png", "cove.png", "chicago.png"]
   end
 
   def create
