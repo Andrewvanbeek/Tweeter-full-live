f931898d2f6ed7f8de7a103c9665813019d5769b
Andrew Van Beek <andrewvanbeek90gmail.com>
2017-01-14 12:48:28 -0600

    got tweets to work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,10 @@
 class GamesController < ApplicationController
   def index
-    tweet_results = $twitter.search("sony -rt", result_type: "recent", lang: "en").to_a.shuffle
+    tweet_results = $twitter.search("nintendo -rt", result_type: "recent", lang: "en").to_a.shuffle
     @tweets = tweet_results.take(tweet_results.length / 2)
+    @tweet_codes = @tweets.map {|tweet| tweet.user.id}
     @backgrounds = ["streetbackground.png", "industrial-background.jpg", "forestpassage.png", "forestbg.png", "BatmanArkhamKnightGothamCity.jpg", "creek.png", "cove.png", "chicago.png"]
   end
 
-  def create
-       ws = Webshot::Screenshot.instance
-       background = ws.capture "http://www.google.com", "google.png", width: 1600, height: 1600, quality: 8
-       @background = background.path
-       puts "HOOOOOOOOOOOOO YEAH"
-      render json: { item: @background.to_s  }
-  end
+
 end
--- app/controllers/twitterfeeds_controller.rb
+++ app/controllers/twitterfeeds_controller.rb
@@ -1,5 +1,11 @@
 class TwitterfeedsController < ApplicationController
   def index
+    if params["tweet"]
+      tweet_id = params['tweet']
+      tweet_user = $twitter.user(tweet_id.to_i)
+      name = tweet_user.screen_name
+      $twitter.update("@#{name} It is Saturday!")
+    end
   end
 
 
